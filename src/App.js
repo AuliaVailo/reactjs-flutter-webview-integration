ed37626c098632116a319d409c12c7da59f10cfa
Moh Abdul Haq Aulia <abdul.haq.aulia@gmail.com>
2022-10-06 15:16:31 +0700

    add sample

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    console.log('register fucntion');
+    window.successPayment = successPayment
+    window.errorPayment = errorPayment
+  })
+  
+  const successPayment = (value) => {
+    alert(value);
+  }
+  
+  const errorPayment = (value) => {
+    alert(value);
+  }
+  
+  const bayar = () => {
+    try {
+      var payoad = {
+        client: "6001",
+        _token: "test",
+        va_number: "74001002198",
+      };
+      window.PayVA.postMessage(JSON.stringify(payoad));
+    } catch (error) {
+      console.log(error);
+      alert(error)
+    }
+  }
+  
+  const back = () => {
+    try {
+      window.BackToHome.postMessage('back-to-home');
+    } catch (error) {
+      console.log(error);
+      alert(error)
+    }
+  }
+
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ReactJS Flutter Integration
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={bayar}>Call Payment Method</button>
+        <br/>
+        <button onClick={back}>Call Back Button</button>
       </header>
     </div>
   );
